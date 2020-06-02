9666a99343c754597735149d643ed5d36c62095a
Sandip <sandiproy10@yahoo.com>
2020-06-02 21:50:33 +0600

    Update Is Unique (Problem 1.1).js

--- Chapter 01/Is Unique (Problem 1.1).js
+++ Chapter 01/Is Unique (Problem 1.1).js
@@ -1,6 +1,6 @@
 /* problem 1.1
 first challenge:  implement an algorithm to determine if a given string has all the unique characters.
-second challenge: what if we are not allowed to use any additional data structure. */
+second challenge:(additional requirement) what if we are not allowed to use any additional data structure. */
 
 //(alternate approach )solution using additional data structure to improve time complexity to 0(n)
 
